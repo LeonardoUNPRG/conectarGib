9d3db7047f19bb70296ea87324c6bcf469d759e1
Jhonatan <Jhonatan@192.168.94.127>
2022-01-18 19:16:03 -0500

    Mi aporte4

--- src/java/Java.java
+++ src/java/Java.java
@@ -18,6 +18,7 @@ public class Java {
         System.out.println("Hola papus");
         System.out.println("Hola Perros");
         System.out.println("Hola Jose");
+        System.out.println("Hola Pastrulos");
     }
     
 }
